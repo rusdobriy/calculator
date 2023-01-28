549b0d125b92eeeb3ec6e44be7eda51849b230f5
rusdobriy <rusdobriy2008@yandex.ru>
2023-01-28 12:18:45 +0300

    'initial'

---
+++ calculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace calculator
+{
+    internal class Program
+
+    {
+        static double InputNumber()
+        {
+            double firstNumber = 0;
+            while (true)
+            {
+                try
+                {
+                    firstNumber = double.Parse(Console.ReadLine());
+                    return firstNumber;
+                }
+                catch
+                {
+                    Console.WriteLine("ублюдок мать твою иди сюда говно вонючее пиши числа!!");
+                }
+            }
+        }
+        static void Main()
+        {   
+            double firstNumber = 0;
+            string action = "";
+            double secondNumber = 0;
+
+            while (true)
+            {
+                Console.WriteLine("напишите первое число");
+                firstNumber = InputNumber();
+
+                Console.WriteLine("напишите математическое действие");
+                action = Console.ReadLine();
+
+                if (action == "#")
+                {
+                    Console.WriteLine("ответ - " + Math.Sqrt(firstNumber));
+                    continue;
+                }
+                if (action == "exit")
+                {
+                    Console.WriteLine("Спасибо за выбор нашей авиакомпании, ждем ваших инвестиций в наше светлое будущее");
+                    return;
+                }
+
+                Console.WriteLine("напишите второе число");
+                secondNumber = InputNumber();
+
+                Actions(firstNumber, action, secondNumber);
+            }
+        }
+        static void Actions(double firstNumber, string action, double secondNumber)
+        {
+            if (action == "/")
+            {
+                if (secondNumber != 0)
+                {
+                    Console.WriteLine("ответ - " + (firstNumber / secondNumber));
+                }
+                else
+                {
+                    Console.WriteLine("Не дели на ноль");
+                }
+            }
+            if (action == "+")
+            {
+                Console.WriteLine("\nответ - " + (firstNumber + secondNumber));
+            }
+
+            if (action == "-")
+            {
+                Console.WriteLine("ответ - " + (firstNumber - secondNumber));
+            }
+            if (action == "*")
+            {
+                Console.WriteLine("ответ - " + (firstNumber * secondNumber));
+            }
+            if (action == "@")
+            {
+                Console.WriteLine("ответ - " + Math.Pow(firstNumber, secondNumber));
+            }
+        }
+    }
+}
---
+++ calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5ed61832-c6a5-4da0-9835-76fe3650e1f3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
